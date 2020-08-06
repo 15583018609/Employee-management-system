4a808bbec6d38678ed18b1adfce172994bddd875
zhu <zhu_demao@163.com>
2020-08-06 22:40:28 +0800

    delete file add.c

--- add.c
+++
@@ -1,8 +0,0 @@
-int add(int a, int b)
-{
-	printf("hello world\n");
-	printf("hello world\n");
-	printf("hello world\n");
-
-	return 10;
-}
--- test.c
+++ test.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+
+int add(int a, int b)
+{
+	return a + b;
+}
+
 int main(int argc, const char *argv[])
 {
+	
+	int i = 5;
+	int j = 6;
+
+	
+
 	printf("hello world\n");
+
+	printf("add = %d\n", add(i, j));
+
 	return 0;
 }
